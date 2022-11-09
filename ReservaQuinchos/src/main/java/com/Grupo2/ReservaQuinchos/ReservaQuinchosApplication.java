a40110ab6fce4378d67118834e156ae33c7dcd0b
Nicolas Casado <nicolascasado@gmail.com>
2022-11-09 12:15:01 -0300

    Subo proyecto inicial

---
+++ ReservaQuinchos/src/main/java/com/Grupo2/ReservaQuinchos/ReservaQuinchosApplication.java
@@ -0,0 +1,13 @@
+package com.Grupo2.ReservaQuinchos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservaQuinchosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservaQuinchosApplication.class, args);
+	}
+
+}
---
+++ ReservaQuinchos/src/test/java/com/Grupo2/ReservaQuinchos/ReservaQuinchosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Grupo2.ReservaQuinchos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservaQuinchosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
